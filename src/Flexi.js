16c49a24a1a737a133a760c11918c9b215ff01c3
Dinesh Tamilselvan <dinesh.tamilselvan@walmartlabs.com>
2019-07-12 22:41:34 +0530

    Data parser

--- src/Flexi.js
+++ src/Flexi.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import FormData from './FormData';
+import CustomFormData from './FormData';
 
 class Flexi extends React.Component {
   constructor(props) {
     super(props);
+    this.formNode = null;
   }
 
-  onSubmit = () => {
+  onSubmit = (e) => {
     // Replace data with composing your form data
-    const data = {};
+    e.preventDefault();
+    const data = Object.values(this.formNode).reduce((obj,field) => {
+      if(field.name) 
+       {
+         obj[field.name] = field.value;
+       }
+      return obj
+    }, {});
+    console.log(data);
     this.props.onSubmit(data); // dont edit this line
   };
 
   render() {
-    console.log(this.props);
     const { items } = this.props.config;
     return (
-      <form>  
+      <form ref={node => (this.formNode = node)}>  
         {
-          <FormData items={items}/>
+          <CustomFormData items={items}/>
         }
         <button onClick={this.onSubmit} />
       </form>
